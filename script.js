7395235d2b27ed550b53ef66f2e93406500f2a63
Mogle7Arkad <george7ali@gmail.com>
2023-05-25 00:37:36 +0100

    make the code play 5 rounds

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function getComputerChoice(){
+    let option = ["Rock", "Paper", "Scissors"];
+    let random = Math.floor(Math.random() * option.length);
+    console.log(random, option[random]);
+};
+getComputerChoice();
+
+//k
+//let option = ["Rock", "Paper", "Scissors"];
+//let randomm = Math.floor(Math.random() * option.length);
+//console.log(randomm, option[randomm]);
+
+//kl
+//const months = ["January", "February", "March"];
+
+//const random = Math.floor(Math.random() * months.length);
+//console.log(random, months[random]);
